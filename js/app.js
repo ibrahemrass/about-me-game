4084d60f03f7847f82c833a143994848b804a149
ibrahemrass <ibrahemrass@gmail.com>
2021-02-08 16:35:38 +0200

    its a grat lap it take 1 hour

---
+++ js/app.js
@@ -0,0 +1,43 @@
+'use strict'
+alert("hello")
+let userName = prompt('what is your name')
+alert(userName)
+console.log('MY NAME IS :', userName);
+let userAge = prompt('are you above 18')
+console.log('THIS IS YOUR AGE:', userAge);
+let fevGame = prompt('do i love gaming')
+console.log(  fevGame);
+let favRaper = prompt('do i like music ')
+console.log(  favRaper);
+let dance = prompt('do i like to dance')
+console.log('i like to dance ', dance)
+let qous1 = prompt('do i like football')
+console.log(qous1)
+let qous2 = prompt('do i live in jordan')
+confirm(userName)
+
+if(qous2.toLowerCase() === 'yes')
+document.write('thats right i live in ramtha')
+else(document.write('i wich not man '))
+
+if(favRaper.toLowerCase() === 'yes' )
+document.write('i love rab music and my favrout raber is eminem')
+else(
+  alert('i love music')
+)
+
+if(dance.toLowerCase() === 'yes')
+document.write('dance is fun')
+else(
+    alert('i love to dance ')
+)
+if(fevGame.toLowerCase() === 'yes')
+document.write('yes i love gaming and my favourt game is overwatch')
+else(
+   alert('you dont know me that will i love gaming')
+)
+ if(qous1.toLowerCase() === 'yes')
+ document.write('i love realmadrid')
+ else(alert('i like tunis to'))
+ 
+
