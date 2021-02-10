01eb214412592c1ff99c7126e8ee24f08cee2c51
areenjaradat <areenjaradat96@gmail.com>
2021-02-10 15:06:25 +0200

    add each question in function in js file modify the README file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # about-me-game
+
+10/2/2021 
+# Lab: 04 - Pair Programming: More extensions to About Me
+
+Areen Jaradat (Driver)
+Ibrahem Aburass(Navigator)
+yhe code was worked on together
--- js/app.js
+++ js/app.js
@@ -1,100 +1,112 @@
- 'use strict'
-let score = 0 
- alert("hello")
- let userName = prompt('what is your name')
- alert(userName)
- console.log('MY NAME IS :', userName);
- let userAge = prompt('are you above 18')
- console.log('THIS IS YOUR AGE:', userAge);
- let fevGame = prompt('do i love gaming')
- console.log(  fevGame);
- let favRaper = prompt('do i like music ')
- console.log(  favRaper);
- let dance = prompt('do i like to dance')
- console.log('i like to dance ', dance)
- let qous1 = prompt('do i like football')
- console.log(qous1)
- let qous2 = prompt('do i live in jordan')
- confirm(userName)
+"use strict";
+let score = 0;
 
- if(qous2.toLowerCase() === 'yes' ){
- document.write('thats right i live in ramtha')
-score++;}
- else(document.write('i live in jordan '))
+alert("hello");
 
+function yourName() {
+  let userName = prompt("what is your name");
+  console.log("YOUR NAME IS :", userName);
+  confirm(userName);
+}
 
- if(favRaper.toLowerCase() === 'yes' ){
- document.write('i love rab music and my favrout raber is eminem')
-score++;}
- else(
-   alert('i love music')
- )
+function question1() {
+  let fevGame = prompt("do i love gaming");
+  console.log(fevGame);
+  if (fevGame.toLowerCase() === "yes" || fevGame.toLowerCase() === "y") {
+    alert("yes i love gaming and my favourt game is overwatch");
+    score++;
+  } 
+  else if (fevGame.toLowerCase() === "no" || fevGame.toLowerCase() === "n") {
+  alert("you dont know me that will i love gaming");
+}
 
- if(dance.toLowerCase() === 'yes'){
+function question2() {
+  let favRaper = prompt("do i like music ");
+  console.log(favRaper);
+  if (favRaper.toLowerCase() === "yes" || favRaper.toLowerCase() === "y" ) {
+    alert("i love rab music and my favrout raber is eminem");
+    score++;
+  } else if (favRaper.toLowerCase() === "no" || favRaper.toLowerCase() === "n" ) { 
+    alert("i love music");
+}
 
-document.write('dance is fun')
-score++;}
- else(
-     alert('i love to dance ')
- )
-if(fevGame.toLowerCase() === 'yes'){
- document.write('yes i love gaming and my favourt game is overwatch')
- score++;}
- else(
-    alert('you dont know me that will i love gaming')
- )
-  if(qous1.toLowerCase() === 'yes'){
+function question3() {
+  let dance = prompt("do i like to dance");
+  console.log("i like to dance ", dance);
+  if (dance.toLowerCase() === "yes" || dance.toLowerCase() === "y") {
+    alert("dance is fun");
+    score++;
+  } else if (dance.toLowerCase() === "no" || dance.toLowerCase() === "n") { 
+    alert("i love to dance ");
+}
 
- document.write('i love realmadrid')
- score++;}
-  else(alert('i like tunis to'))
- 
+function question4() {
+  let qous1 = prompt("do i like football");
+  console.log(qous1);
 
+  if (qous1.toLowerCase() === "yes" || qous1.toLowerCase() === "y") {
+    alert("i love realmadrid");
+    score++;
+  } else if (qous1.toLowerCase() === "no" || qous1.toLowerCase() === "n") {
+     alert("i like tunis to");}
+  
+}
 
+function question5() {
+  let qous2 = prompt("do i live in jordan");
+  if (qous2.toLowerCase() === "yes" || qous2.toLowerCase() === "y") {
+    alert("thats right i live in ramtha");
+    score++;
+  } else  if (qous2.toLowerCase() === "no" || qous2.toLowerCase() === "n") {
+    alert("i live in jordan ");}
+}
+
+function question6() {
 let uesrGuss;
 let myNum = 15;
 for (let i = 0; i < 4; i++) {
-   uesrGuss = prompt('please Choose a numper between 0-30');
-  if(parseInt(uesrGuss) === myNum){
-    alert('thats right');
+  uesrGuss = prompt("please Choose a numper between 0-30");
+  if (parseInt(uesrGuss) === myNum) {
+    alert("thats right");
     score++;
     break;
+  } else if (parseInt(uesrGuss) > myNum) {
+    alert("to high");
+  } else {
+    alert("to low");
   }
-  else if(parseInt(uesrGuss) > myNum){
-    alert('to high');
+  if (i === 3) {
+    alert("the right answer is 15");
   }
-  else{
-    alert('to low');
-  }
-  if(i===3){
-    alert('the right answer is 15')
-  }
-  
 }
- let useranswr;
- let arrOfColer =['red', 'green', 'blou', 'gry','black']
+}
 
- 
- let index;
- let flag = false
- for(index=0 ; index <6 ; index++) {
-  useranswr = prompt('can u guss what colers do i like');
-  useranswr=useranswr.toLowerCase();
-  for(let j =0; j<arrOfColer.length;j++) {
-    alert('thats right');
+let arrOfColer = ["red", "green", "blou", "gry", "black"];
+
+function question7() {
+let useranswr;
+let index;
+let flag = false;
+for (index = 0; index < 6; index++) {
+  useranswr = prompt("can u guss what colers do i like");
+  useranswr = useranswr.toLowerCase();
+  for (let j = 0; j < arrOfColer.length; j++) {
+    alert("thats right");
     flag = true;
     score++;
     break;
   }
-  if(flag){
+  if (flag) {
     break;
   }
-
- 
-  }
- alert(`my favourite colers ${arrOfColer} and your scoer is  ${score}`)
- 
-
- 
-  
-
+}
+}
+yourName();
+question1();
+question2();
+question3();
+question4();
+question5();
+question6();
+question7();
+alert(`my favourite colers ${arrOfColer} and your score is  ${score}`)
